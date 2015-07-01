71b83461ea27bd1cda4c2a89c46abd695ba7bf8e
mtgomez <mtgomez@mtgomezs-MacBook-Pro-3.local>
2015-06-30 23:41:11 -0700

    commit attempt

--- script.js
+++ script.js
@@ -1,42 +1,76 @@
 $(function() {
 
-	var $newItem = $('#new-item');
-	var	$itemName = $('#item-name');
-	var $taskitem = $('.taskitem');
-	var $description = $('#description');
-	var $done = $('#done');
-//	$newItem.submit(function(){
-//
-//	}); 
+  // form to create new todo
+  var $newToDo = $('#new-todo');
 
+  // element to hold our list of todos
+  var $toDoList = $('#todo-list');
 
+  // todo template
+  var toDoTemplate = _.template($('#todo-template').html());
 
-	$newItem.on('submit', function(event){
-		console.log("submitted form");
-		event.preventDefault();
-		console.log($itemName.val());
+  // `toDos` array is our model (holds our data)
+  // contains test (or "seed") data
+  var toDos = [
+    {name: "laundry", desc: "clean clothes"},
+    {name: "grocery shopping", desc: "buy food"},
+    {name: "nap time", desc: "remember to sleep!"}
+  ];
 
-		if ( $itemName.val() ==="" || $description.val() === ""){
-			alert("empty field");
-			}
-		
- 		else{
-        	$("#tasks").append("<li class='taskitem'>" +"Title:  "+ $itemName.val() + "<p>"+ "Description:  " + $description.val() + "</p><div class='editform'><div class='form-group'><input type='text' class='form-control' id='new-item-name' placeholder='edit title' autofocus></div><div class='form-group'><input type='text' class='form-control' id='newdescription' placeholder='edit description' autofocus></div></div><input type='submit' class='btn btn-success editbtn' value='Edit'><button id='done' type='button' class='btn btn-default'>Done</button></li>");
-			var $taskitem = $('.taskitem');     	
-     		$('#new-item').trigger("reset");
-     		var $taskitem = $('.taskitem');
-     		}
-          });
+  // append existing todos (from seed data) to `$toDoList`
+  // `_.each` is an "iterator" function provided by Underscore.js
+  _.each(toDos, function (todo, index) {
+    var $todo = $(toDoTemplate(todo));
+    $todo.attr('data-index', index);
+    $toDoList.append($todo);
+  });
 
-	$("button").on('click', function(event) {
-      $(this).parent().remove();
-    
+  // submit form to create new todo
+  $newToDo.on('submit', function(event) {
+    event.preventDefault();
+
+    // create new todo object from form data
+    var toDoName = $('#todo-name').val();
+    var toDoDesc = $('#todo-desc').val();
+    var toDoData = {name: toDoName, desc: toDoDesc};
+
+    // store our new todo
+    toDos.push(toDoData);
+    console.log(toDos);
+    var index = toDos.indexOf(toDoData);
+
+    // append our new todo to the page
+    var $todo = $(toDoTemplate(toDoData));
+    $todo.attr('data-index', index);
+    $toDoList.append($todo);
+
+    // reset the form
+    $newToDo[0].reset();
+    $('#todo-name').focus();
+  });
+
+  // add class to todo on click to mark it as done
+  $toDoList.on('click', '.todo-text', function() {
+    $(this).toggleClass('done');
+  });
+
+  // remove todo from model and view
+  $toDoList.on("click", ".delete", function () {
+    var $todo = $(this).closest(".todo");
+    var index = $todo.attr('data-index');
+
+    // remove todo from the `toDos` array (model)
+    toDos.splice(index, 1);
+    console.log(toDos);
+
+    // remove todo from the DOM (view)
+    $todo.remove();
+
+    // reset indexes in DOM to match `toDos` array
+    // $.each loops through DOM elements
+    $('.todo').each(function(index) {
+      $(this).attr('data-index', index);
     });
-
-    $('.editform').hide(); //Initially form wil be hidden.
-  	$('.editbtn').click(function() {
-    $('.editform').toggle();//Form toggles on button click
-    });
+  });
 
 });
-
